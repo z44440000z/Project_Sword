f1cf67e612485fbe780e8d772eee14565a89df97
Xnethers <z44440000z@gmail.com>
2019-12-31 01:12:45 +0800

    極.簡易方塊生成

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,15 +7,24 @@ public class GameManager : MonoBehaviour
     public GameObject A;
     public GameObject B;
 
+    public int StartCount = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < StartCount; i++)
+        {
+            if (Random.Range(0, 2) == 0)
+            { Instantiate(A, transform.position + new Vector3(0, i * 3, 0), transform.rotation); }
+            else
+            { Instantiate(B, transform.position + new Vector3(0, i * 3, 0), transform.rotation); }
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
